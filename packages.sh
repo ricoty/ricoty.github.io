365ca1fc9e489f728be0a711fb66cec35177dc7c
ricoty <w.wansleeben@gmail.com>
2016-08-29 20:02:22 +0000

    added pokepatch and poke++ deb's

--- packages.sh
+++ packages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/iPokeGo debs
+dpkg-deb -bZgzip projects/PokeGo++ debs
+dpkg-deb -bZgzip projects/PokePatch debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
